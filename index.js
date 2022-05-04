38eda449929d81677c8e0593ec2256f43b6ae3a9
Anthony Stainton <anthonystainton@gmail.com>
2022-05-04 06:35:16 +0000

    init

---
+++ index.js
@@ -0,0 +1,24 @@
+/*
+    Hue v4.0
+    Its finally here.
+
+    Written by Anthony Stainton.
+    Github: https://github.com/ItzRock
+    Discord: Anthоny#0001 (299682971374452739)
+
+    Support Server: https://discord.gg/QwgnZ83XD3
+
+    ----------------------
+
+    Based off of Hue v3.0
+    https://github.com/ItzRock/Hue-v3.0/
+         ↳ Contributor(s) of Hue v3.0
+            Anthony Stainton (https://github.com/ItzRock)
+            Harry Chen (https://github.com/HarryXChen3)
+            
+
+    Contributor(s) of Hue v4.0
+    Anthony Stainton (https://github.com/ItzRock)
+
+*/
+const discord = require("discord.js");
